7e79ad85ef2c39129b013bfa1108c3281cfdfcb5
KortaPo <70193867+KortaPo@users.noreply.github.com>
2022-04-13 14:52:46 +0530

    Better documentation, updated imports, and added return statements to functions that were missing it

--- src/endpoints.py
+++ src/endpoints.py
@@ -4,17 +4,21 @@ from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi_limiter import FastAPILimiter
 from fastapi_limiter.depends import RateLimiter
+
 from src.app import DetectionAPI
-from utils.helpers import ImageRequest, TextRequest, Token, OCRData
+from utils.models import ImageRequest, OCRData, TextRequest, Token
 
 app = DetectionAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.on_event("startup")
-async def startup():
+async def startup() -> None:
     """
-    This method is called when the server starts up, it initializes a redis connection.
+    |coro|
+    This method is triggered when the FastAPI app instance starts up, it is binded to the event named as
+    `startup` in the above listener (decorator).
+    This function initializes the redis connection.
     """
     redis = await aioredis.from_url(
         url=app.config.redis_address,
@@ -26,25 +30,24 @@ async def startup():
         decode_responses=True,
     )
     await FastAPILimiter.init(redis)
+    return
 
 
 @app.on_event("shutdown")
-async def shutdown():
+async def shutdown() -> None:
     """
-    This method is called when the server shuts down.
+    |coro|
+    This method is binded to the shutdown event of the server triggered when the FastAPI app instance shuts down,
+    it closes the redis connection.
     """
     await FastAPILimiter.close()
+    return
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root():
+async def read_root() -> HTMLResponse:
     """
-    Reads the root of the server.
-
-    Returns
-    -------
-    HTMLResponse
-        The root of the server.
+    This endpoint is the root endpoint of the API.
     """
     return HTMLResponse(
         content=open("static/index.html", "r").read(),
@@ -53,14 +56,9 @@ async def read_root():
 
 
 @app.get("/game", response_class=HTMLResponse)
-async def game():
+async def game() -> HTMLResponse:
     """
-    This endpoint loads a game. Visit the endpoint for more information.
-
-    Returns
-    -------
-    HTMLResponse
-        The root of the server.
+    This endpoint contains an experimental web based game.
     """
     return HTMLResponse(
         content=open("static/game.html", "r").read(),
@@ -70,7 +68,7 @@ async def game():
 
 @app.post(
     "/token/image/{url}",
-    dependencies=[Depends(RateLimiter(times=1, seconds=20))],
+    dependencies=[Depends(RateLimiter(times=1, seconds=30))],
     response_model=Token,
 )
 async def read_token_from_image(
@@ -79,6 +77,7 @@ async def read_token_from_image(
     """
     This endpoint reads an image from an url and tries extract the token from it, this uses tesseract-ocr, so it might
     not be accurate all the time.
+    This endpoint has a rate limiter, so it can only be called once every 20 seconds.
 
     Parameters
     ----------
@@ -96,7 +95,7 @@ async def read_token_from_image(
 
 @app.post(
     "/token/text/{text}",
-    dependencies=[Depends(RateLimiter(times=3, seconds=20))],
+    dependencies=[Depends(RateLimiter(times=1, seconds=5))],
     response_model=Token,
 )
 async def read_token_from_text(
@@ -105,6 +104,7 @@ async def read_token_from_text(
     """
     This endpoint reads a text and tries extract a token from it, if found, it will return the token and various
     other information related to the token.
+    This endpoint has a rate limiter, so it can only be called once every 5 seconds.
 
     Parameters
     ----------
@@ -121,15 +121,13 @@ async def read_token_from_text(
 
 @app.post(
     "/ocr/text/{text}",
-    dependencies=[Depends(RateLimiter(times=3, seconds=20))],
+    dependencies=[Depends(RateLimiter(times=1, seconds=30))],
     response_model=OCRData,
 )
-async def OCR_endpoint(
-    data: ImageRequest,
-) -> JSONResponse:
+async def OCR_endpoint(data: ImageRequest) -> JSONResponse:
     """
-    This enpoint takes an url of an image and tries to extract the text from it. The extracted text will be not
-    100% accurate. It uses tesseract-ocr, so it might not be accurate all the time.
+    This enpoint takes an url of an image, validates and downloads the image and returns the text extracted from it as
+    OCRData response model. This endpoint uses Tesseract OCR engine to process the image.
 
     Parameters
     ----------
